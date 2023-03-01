b906254fe3841f73d5b779ab5e2122684aefeb06
Marcus Rangel <marcus.vbrangel@gmail.com>
2023-03-01 20:10:54 -0300

    refactor: create a new module for the angular material

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,12 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
+import { AppAngularMaterialModule } from '../shared/app-angular-material/app-angular-material.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses.component';
-
 
 @NgModule({
   declarations: [
@@ -15,9 +12,8 @@ import { CoursesComponent } from './courses.component';
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule,
-    MatCardModule,
-    MatToolbarModule
+    AppAngularMaterialModule
   ]
 })
+
 export class CoursesModule { }
