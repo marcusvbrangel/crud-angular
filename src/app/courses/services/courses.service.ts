9e1cdb9c68038151a599475fa96be1806fd3c9d0
Marcus Rangel <marcus.vbrangel@gmail.com>
2023-03-05 19:12:05 -0300

    feat: create a spinner to courses page

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,26 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, first } from 'rxjs/operators';
+import { delay, first, tap } from 'rxjs/operators';
 
 import { Course } from '../model/course';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class CoursesService {
 
-  private readonly API = '/assets/courses.json';
+    private readonly API = '/assets/courses.json';
 
-  constructor(private httpClient: HttpClient) { }
+    constructor(private httpClient: HttpClient) { }
 
-  list() {
+    list() {
 
-    return this.httpClient.get<Course[]>(this.API)
-    .pipe(
-        first(),  // Note: Use this method because the call isn't a stream... just the first result is necessary...
-        tap(courses => console.log(courses))
-    );
+        return this.httpClient.get<Course[]>(this.API)
+            .pipe(
+                first(),  // Note: Use this method because the call isn't a stream... just the first result is necessary...
+                delay(1000),  // Note: one second delay...
+                tap(courses => console.log(courses))
+            );
 
-  }
+    }
 
 }
