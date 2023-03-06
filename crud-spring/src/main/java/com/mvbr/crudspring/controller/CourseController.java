168e4a34942e50ce91682c55aa08518047102e25
Marcus Rangel <marcus.vbrangel@gmail.com>
2023-03-06 20:40:37 -0300

    feat: create course repository

--- crud-spring/src/main/java/com/mvbr/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/mvbr/crudspring/controller/CourseController.java
@@ -1,6 +1,7 @@
 package com.mvbr.crudspring.controller;
 
 import com.mvbr.crudspring.model.Course;
+import com.mvbr.crudspring.repository.CourseRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +12,15 @@ import java.util.List;
 @RequestMapping("/api/v1/courses")
 public class CourseController {
 
+    private final CourseRepository courseRepository;
+
+    public CourseController(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
     @GetMapping
     public List<Course> list() {
-        return null;
+        return courseRepository.findAll();
     }
 
 }
--- crud-spring/src/main/java/com/mvbr/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/mvbr/crudspring/model/Course.java
@@ -1,12 +1,10 @@
 package com.mvbr.crudspring.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity
 public class Course {
 
     @Id
