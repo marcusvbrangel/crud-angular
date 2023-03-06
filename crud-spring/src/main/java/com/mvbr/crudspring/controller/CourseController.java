ee92c267de03e4fd84dddc992446e262338a150a
Marcus Rangel <marcus.vbrangel@gmail.com>
2023-03-06 19:48:02 -0300

    feat: create course model

--- crud-spring/src/main/java/com/mvbr/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/mvbr/crudspring/controller/CourseController.java
@@ -1,18 +1,19 @@
 package com.mvbr.crudspring.controller;
 
+import com.mvbr.crudspring.model.Course;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api")
-public class HelloController {
+@RequestMapping("/api/v1/courses")
+public class CourseController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, world!";
+    @GetMapping
+    public List<Course> list() {
+        return null;
     }
-
-
 
 }
---
+++ crud-spring/src/main/java/com/mvbr/crudspring/model/Course.java
@@ -0,0 +1,22 @@
+package com.mvbr.crudspring.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(length = 100, nullable = false)
+    private String category;
+
+}
